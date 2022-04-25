1567bb1f451d785e9e38886a063a69548478f6fc
Umbalite <100040819+Umbalite@users.noreply.github.com>
2022-04-25 17:57:13 -0400

    Add files via upload

---
+++ JS/Password.js
@@ -0,0 +1,66 @@
+// Assignment code here
+var characterLength= 8;
+var choiceArr= [];
+
+var specialCharArr= ['!','@','#','$','%','^','&','*','(',')',];
+var lowerCaseArr= ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',];
+var upperCaseArr= ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',];
+var numberArr= ['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20',];
+//Went a bit longer on the number array. Can add more special characters if needed, simply went with top row since it's most common for most internet users.//
+
+var generateBtn = document.querySelector("#generate");
+//Add event listener //
+generateBtn.addEventListener("click", writePassword);
+// Write password to the #password input
+function writePassword() {
+  var correctPrompts= getPrompts(); 
+  
+  if(correctPrompts) {
+   var newPassword = generatePassword();
+   var passwordText = document.querySelector("#password");
+   passwordText.value = newPassword;
+  }else {
+    passwordTest.value= "";
+  } 
+ 
+ }
+
+
+
+
+function generatePassword(){
+  var password= "";
+  for(var i = 0; i< characterLength; i++){
+    var randomIndex = Math.floor(Math.random() * choiceArr.length);
+    password= password + choiceArr[randomIndex];
+  }
+  return password;
+}
+
+function getPrompts() {
+  choiceArr=[];
+  
+  characterLength= parseInt(prompt("How many characters would you like your password to be? (8-128)"));
+  if(isNaN(characterLength) ||characterLength< 8 || characterLength > 128) {
+    alert("Character length must be 8-128 characters only.");
+    return false;
+  }
+
+
+if (confirm("Would you like lowercase letters in the password?")){
+  choiceArr= choiceArr.concat(lowerCaseArr);
+}
+
+if (confirm("Would you like uppercase letters in the password?")){
+  choiceArr= choiceArr.concat(upperCaseArr);
+}
+
+if (confirm("Would you like special characters in the password?")){
+  choiceArr= choiceArr.concat(specialCharArr);
+}
+
+if (confirm("Would you like numbers in the password?")){
+  choiceArr= choiceArr.concat(numberArr);
+}
+return true;
+}
